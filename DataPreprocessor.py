53e4d3e5e59102f999274cda13ec359a747b132e
anerirana <anerirana2000@gmail.com>
2021-02-27 15:06:21 +0530

    logic for removing duplicate tweets

---
+++ DataPreprocessor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+TWEETS_FILE_PATH = './Data/Tweets.csv'
+
+
+def remove_duplicate_tweets():
+    df = pd.read_csv(TWEETS_FILE_PATH, index_col=0)
+    num_rows = len(df)
+
+    # Drop rows with same media ID
+    df.drop_duplicates(subset='MediaID', inplace=True)
+    print("Dropped ", (num_rows-len(df)),
+          " media ID duplicates")  # to-do: log info
+    num_rows = len(df)
+
+    # Drop rows with same tweet URL
+    df.drop_duplicates(subset='TweetURL', inplace=True)
+    print("Dropped ", (num_rows-len(df)),
+          " tweet URL duplicates")  # to-do: log info
+
+    df = df.reset_index(drop=True)
+    df.to_csv(TWEETS_FILE_PATH)
+
+
+remove_duplicate_tweets()
