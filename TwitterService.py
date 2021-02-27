f63e530724c8c30b29c915c8422b9f32269e18fc
sonalijha108 <43270689+sonalijha108@users.noreply.github.com>
2021-02-27 13:02:17 +0530

    Added exception handling

--- TwitterService.py
+++ TwitterService.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
-import os
 import requests
 from TweetProcessor import TweetParser
 
 BEARER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAAFZkIwEAAAAAukM9PY5a8Z0K1U3X4frDp%2BeZX4o%3Dv2Ir1HreMt9HvqjKJWaWT6Jx7gWnbCkKyiDbANzeqfQ84B2piL"
-FILENAME = "/content/drive/MyDrive/OffensiveLanguageClassification/sensiive_keywords.txt"
-URL = "https://api.twitter.com/1.1/tweets/search/fullarchive/HushUp.json?tweet_mode='extended'"
-KEYWORD_FILE_PATH = "./Data/Keywords.txt"
 
 
 def get_keyword_string():
@@ -17,13 +13,14 @@ def get_keyword_string():
     file_obj = open(KEYWORD_FILE_PATH, "r")
     keywords = file_obj.readlines()
     for keyword in keywords:
+        # keyword = keyword[:-1]
         keyword_string = keyword_string+str(keyword)+str(whitespace)
     return keyword_string
 
 
 def fetch_tweets(keyword_string):
     payload1 = '{"query":"'
-    payload2 = 'has:videos lang:en","maxResults":"15"}'
+    payload2 = 'has:videos","maxResults":"15"}'
     payload = str(payload1)+str(keyword_string)+str(payload2)
     print(payload)
     headers = {"Authorization": BEARER_TOKEN}
@@ -33,7 +30,14 @@ def fetch_tweets(keyword_string):
     return json_response
 
 
-keyword_string = get_keyword_string()
-response = fetch_tweets(keyword_string)
-tweet_parser = TweetParser(response["results"], debug_scope=0)
-tweet_parser.store_tweet_data()
+try :
+    keyword_string = get_keyword_string()
+    response = fetch_tweets(keyword_string)
+    tweet_parser = TweetParser(response["results"], debug_scope=0)
+    tweet_parser.store_tweet_data()
+except KeyError:
+    print("Error fetching tweets")
+    print(response["error"])
+except Exception as e:
+    print("Unknown exception")
+    print(e)
