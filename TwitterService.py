f0b8a166653adddb48a4f107be4ba4b2977c252c
anerirana <anerirana2000@gmail.com>
2021-03-06 14:27:23 +0530

    adding test env for twitter service

--- TwitterService.py
+++ TwitterService.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import os
+import argparse
 import requests
 from TweetProcessor import TweetParser
 import CustomLogger
 
 BEARER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAAAA2JQEAAAAA2mlhZpa5zBG2PqvL%2BGoADWDO5GA%3Dk50Ddhm8r7GsCNUF3uhgX95BTbMCgpAPQhn8ds1o9KBtp6HUD6"
-URL = "https://api.twitter.com/1.1/tweets/search/fullarchive/development.json?tweet_mode='extended'"
+TEST_URL = "https://api.twitter.com/1.1/tweets/search/30day/test.json?tweet_mode='extended'"
+DEV_URL = "https://api.twitter.com/1.1/tweets/search/fullarchive/development.json?tweet_mode='extended'"
 KEYWORD_FILE_PATH = "./Data/Keywords.txt"
 logger = CustomLogger.getCustomLogger()
 
@@ -17,6 +19,7 @@ def get_keyword_string():
     closing_bracket = ")"
     keyword_string = opening_bracket
     whitespace = " "
+    negation_keyword = "-porn "
 
     file_obj = open(KEYWORD_FILE_PATH, "r")
     keywords = file_obj.readlines()
@@ -26,7 +29,7 @@ def get_keyword_string():
         keyword = keyword[:-1]
         logger.info("keyword after trimming the last character : %s", keyword)
         if i == num_keywords - 1:
-          keyword_string = keyword_string+str(keyword)+str(closing_bracket)+str(whitespace)
+          keyword_string = keyword_string+str(keyword)+str(closing_bracket)+str(whitespace)+negation_keyword
         else:
           keyword_string = keyword_string+str(keyword)+str(whitespace)+"OR"+str(whitespace)
     logger.info("Keyword string generated : %s", keyword_string)
@@ -34,7 +37,7 @@ def get_keyword_string():
     return keyword_string
 
 
-def fetch_tweets(keyword_string, next_token):
+def fetch_tweets(url, keyword_string, next_token):
     logger.debug("Start : fetch_tweets(%s,%s)", keyword_string, next_token)
     payload1 = '{"query":"'
     payload2 = 'has:videos lang:en","maxResults":"15"'
@@ -46,7 +49,7 @@ def fetch_tweets(keyword_string, next_token):
       payload = str(payload1)+str(keyword_string)+str(payload2)+str(payload4)
     logger.info("Payload : %s", payload)
     headers = {"Authorization": BEARER_TOKEN}
-    response = requests.post(URL, data=payload, headers=headers)
+    response = requests.post(url, data=payload, headers=headers)
     json_response = response.json()
     logger.info("Dictionary keys from response:")
     logger.info(json_response.keys())
@@ -57,32 +60,43 @@ def fetch_tweets(keyword_string, next_token):
     logger.debug("End : fetch_tweets(%s,%s)", keyword_string, next_token)
     return json_response
 
-curr_next=''
-try :
-    keyword_string = get_keyword_string()
-    response = fetch_tweets(keyword_string, None)
-    tweet_parser = TweetParser(response["results"])
-    tweet_parser.store_tweet_data()
-    if "next" in response.keys():
-        next_token = response["next"]
-        curr_next = next_token
-    else :
-        next_token = None
-    while next_token :
-        logger.debug("Getting tweets from the next page with the token : %s", next_token)
-        curr_next = next_token
-        response = fetch_tweets(keyword_string, next_token)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--env', dest='env', default='test', help='The environment for twitter endpoint. Right now only \'dev\' and \'test\' are supported')
+    args = parser.parse_args()
+    url = ''
+    if args.env == 'dev':
+        url = DEV_URL
+    elif args.env == 'test':
+        url = TEST_URL
+
+    curr_next=''
+    try :
+        keyword_string = get_keyword_string()
+        response = fetch_tweets(url, keyword_string, None)
         tweet_parser = TweetParser(response["results"])
         tweet_parser.store_tweet_data()
         if "next" in response.keys():
             next_token = response["next"]
-        else : 
+            curr_next = next_token
+        else :
             next_token = None
-    logger.debug("Logging end of tweet search; no further next token found")
-except KeyError:
-    logger.error("Logging next_token : %s", curr_next)
-    logger.error("Error fetching tweets", exc_info=True)
-    logger.error(response["error"])
-except Exception:
-    logger.error("Logging next_token : %s", curr_next)
-    logger.error("Unknown Exception", exc_info=True)
+        while next_token :
+            logger.debug("Getting tweets from the next page with the token : %s", next_token)
+            curr_next = next_token
+            response = fetch_tweets(url, keyword_string, next_token)
+            tweet_parser = TweetParser(response["results"])
+            tweet_parser.store_tweet_data()
+            if "next" in response.keys():
+                next_token = response["next"]
+            else : 
+                next_token = None
+        logger.debug("Logging end of tweet search; no further next token found")
+    except KeyError:
+        logger.error("Logging next_token : %s", curr_next)
+        logger.error("Error fetching tweets", exc_info=True)
+        logger.error(response["error"])
+    except Exception:
+        logger.error("Logging next_token : %s", curr_next)
+        logger.error("Unknown Exception", exc_info=True)
